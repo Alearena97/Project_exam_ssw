687e88388299d1506e1585d9bcfa3103d4d64d58
Alearena97 <72215704+Alearena97@users.noreply.github.com>
2023-05-26 11:57:26 +0200

    2 files modified

--- src/root/ricerca/ricerca.component.ts
+++ src/root/ricerca/ricerca.component.ts
@@ -16,7 +16,8 @@ import { Libreria } from '../libreria';
 export class RicercaComponent {
   // stringa per l'errore 
   errore : string = '';
-  libreria : Libreria[] = []
+  // dichiarazione dell'array che conterrà i risultati filtrati
+  elenco_libri : Array<Documento> = []
 
   constructor(private bs: BibliotecaService) {}
   ngOnInit() {}
@@ -25,25 +26,26 @@ export class RicercaComponent {
     //inizializzazione variabili tramite input 
     var tit_aut: HTMLInputElement = document.getElementById("Titolo_Autore") as HTMLInputElement;
     //var libreria : Libreria.archivio = []
-
-    var newArchive : Array<Documento>
+    // controllo che se l'input è vuoto l'elenco libri venga svuotato per non mostrarli 
+    if (tit_aut.value == "") {
+      this.elenco_libri = [];
+      return;}
     // richiedo l'archivio vuoto
     this.bs.getDocument().subscribe({
       next: (x: AjaxResponse<any>) => {
 
-      console.log(tit_aut.value);
+     // console.log(tit_aut.value);
       //associo ad una variabile l'array di documenti scaricato e lo rendo una stringa di tipo JSON 
       var newArchive = JSON.parse(x.response)
 
       // creo la libreria filtrando l'array di documenti con l'input inserito nella stringa 
-      var libreria : Libreria = newArchive.filter((doc:Documento)  => (doc.titolo+doc.autore).toLowerCase().includes(tit_aut.value));
-      console.log(libreria);
-      // controllo che i campi non siano vuoti
-      if (libreria.archivio.length == 0) {
-      this.errore = 'Riempi tutti i campi prima di inserire un documento!';
+      var libreria: Libreria = new Libreria(newArchive)
+      this.elenco_libri = libreria.archivio.filter((doc:Documento)  => (doc.titolo+doc.autore).toLowerCase().includes(tit_aut.value));
+      //console.log(this.elenco_libri);
+      // controllo che ci siano risultati
+      if (this.elenco_libri.length == 0) {
+      this.errore = 'Nessun risultato trovato.';
       return;}
-
-      console.log(libreria);
     },
       error: (err) =>
         console.error('Observer got an error: ' + JSON.stringify(err))
