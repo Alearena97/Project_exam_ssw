13d1a78608d9c71c0c06c4fbc75a00d1047839f0
Alearena97 <72215704+Alearena97@users.noreply.github.com>
2023-05-29 17:29:18 +0200

    3 files modified

--- src/root/ricerca/noleggia/noleggia.component.ts
+++ src/root/ricerca/noleggia/noleggia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BibliotecaService } from '../../biblioteca.service';
 import { ajax, AjaxResponse } from 'rxjs/ajax';
@@ -13,11 +13,57 @@ import { Libreria } from '../../libreria';
   imports: [ CommonModule],
   standalone: true
 })
-export class NoleggiaComponent implements OnInit {
+export class NoleggiaComponent {
+  // stringa per l'errore
+  errore: string = '';
 
-  constructor() { }
+  @Input() libro_selezionato: Documento = new Documento('', '', '', '');
+  // mando fuori un evento che comunica un array di documenti (libreria_update) e un messaggio
+  @Output() onUpdate_delete = new EventEmitter<{message:string,update:Documento[]}>();
+  constructor(private bs: BibliotecaService) {}
+  ngOnInit() {}
 
-  ngOnInit() {
+  NoleggiaDocument() {
+
+    //Inserisco un controllo che non permette di eliminare un documento già noleggiato con un timeout
+
+    var prestatario: HTMLInputElement = document.getElementById("Titolo_Autore") as HTMLInputElement;
+
+
+    if (this.libro_selezionato.noleggiatore != "Disponibile") {
+    this.errore = 'Non è possibile noleggiare un documento già noleggiato';
+    setInterval (() => {
+    this.errore = ''
+    },3000)
+    return;
+    }
+
+    // richiedo l'archivio vuoto
+    this.bs.getDocument().subscribe({
+      next: (x: AjaxResponse<any>) => {
+        //associo ad una variabile l'array di documenti scaricato e lo rendo una stringa di tipo JSON
+        var newArchive = JSON.parse(x.response);
+        let libreria: Libreria = new Libreria(newArchive);
+        // creo la libreria con l'elenco dei libri controllando tramite filter tutti gli elementi che hanno posizione diversa da quella selezionata
+        var libreria_update = libreria.archivio.filter(
+          (doc: Documento) => doc.posizione != this.libro_selezionato.posizione
+        );
+
+        //ricarico la nuova libreria tramite la SET
+        this.bs.setDocument(libreria_update).subscribe({
+          next: (x: AjaxResponse<any>) => {},
+          error: (err) =>
+            console.error('Observer got an error: ' + JSON.stringify(err)),
+        });
+        //dopo aver eliminato emetto un evento che contiene l'array e un messaggio 
+        this.onUpdate_delete.emit({message: 'Documento eliminato!',update: libreria_update})
+      },
+      error: (err) =>
+        console.error('Observer got an error: ' + JSON.stringify(err)),
+    });
+
+    //svuoto i campi
+
+    this.errore = '';
   }
-
 }
